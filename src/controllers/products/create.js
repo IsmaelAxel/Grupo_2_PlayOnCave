c61e4e75a54ef7ded995eb3d44971fb797be4cf1
IsmaelAxel <ismael_callamullo@hotmail.com>
2023-08-25 11:07:18 -0300

    feat: create_refactorizado

---
+++ src/controllers/products/create.js
@@ -0,0 +1,30 @@
+const {writeJSON,readJSON} = require('../../data')
+const { v4: uuidv4 } = require('uuid');
+module.exports = (req,res) => {
+    const products = readJSON('products.json')
+    products.push({
+        id: uuidv4(),
+        title: req.body.title,
+        category: req.body.category,
+        price: +req.body.price,
+        discount: +req.body.discount,
+        MainImage : req.files.MainImage ? req.files.MainImage[0].filename : null,
+        images : req.files.images ? req.files.images.map(image => image.filename) : [],
+        description: req.body.description.trim(),
+        reqRecommendedOs: req.body.reqRecommendedOs.trim(),
+        reqMinOs: req.body.reqMinOs.trim(),
+        reqRecommendedProcessor: req.body.reqRecommendedProcessor.trim(),
+        reqMinProcessor: req.body.reqMinProcessor.trim(),
+        reqRecommendedMemory: req.body.reqRecommendedMemory.trim(),
+        reqMinMemory: req.body.reqMinMemory.trim(),
+        reqRecommendedGraphicsCard: req.body.reqRecommendedGraphicsCard.trim(),
+        reqMinGraphicsCard: req.body.reqMinGraphicsCard.trim(),
+        reqRecommendedDisk: req.body.reqRecommendedDisk.trim(),
+        reqMinDisk: req.body.reqMinDisk.trim()
+    }
+    )
+    writeJSON(products, 'products.json')
+    return res.redirect('/')
+   
+    
+}
