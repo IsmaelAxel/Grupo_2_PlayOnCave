b25e2ec5205a14847c6edb207ab35be79e4baace
Gaston Gomez <gaston.miguel.gomez@gmail.com>
2023-08-28 18:22:33 -0300

    feat: dashboard admin

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,3 +1,7 @@
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const { writeJSON, readJSON } = require('../data/index');
+const productRead = readJSON('products.json');
+
 module.exports = {
     login: (req, res) => {
         return res.render('login');
@@ -5,8 +9,13 @@ module.exports = {
     register: (req, res) => {
         return res.render('register');
     },
-    admin: (req, res) => {
-        return res.render('admin');
+    admin : (req,res)  => {
+
+        const products = readJSON('products.json');
+        return res.render('admin', {
+            products,
+            
+        })
     }
 
 }
