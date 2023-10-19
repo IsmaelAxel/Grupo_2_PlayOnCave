d962ee779323e06558f959b2db7df4e530de5881
Wilsonc7 <wolfc777@gmail.com>
2023-10-19 20:14:08 -0300

    fixs model and migration section

--- src/database/migrations/20231016224147-create-section.js
+++ src/database/migrations/20231016224147-create-section.js
@@ -15,9 +15,6 @@ module.exports = {
       image: {
         type: Sequelize.STRING
       },
-      productsId: {
-        type: Sequelize.STRING
-      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
