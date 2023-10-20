ad6da19e6bebaa3c2ac8273ca2e0ffc236232b65
IsmaelAxel <ismael_callamullo@hotmail.com>
2023-10-20 12:31:02 -0300

    feat: delete terminado

--- src/controllers/products/delete.js
+++ src/controllers/products/delete.js
@@ -1,36 +1,25 @@
 const db = require('../../database/models');
+const {existsSync, unlinkSync} = require('fs')
 module.exports = (req,res) => {
-    db.Products.destroy({
-        where: {
-            id: req.params.id
+    db.Images.findAll({
+        where:{
+            productId: req.params.id
         }
-    }).then(() => {
-        return res.redirect('/users/admin')
+    }).then(images =>{
+        images.forEach(image => {
+            existsSync(`./public/images/products/${image.file}`) &&
+            unlinkSync(`./public/images/products/${image.file}`);
+        });
+        db.Products.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(() => {
+            return res.redirect('/users/admin')
+        })
     })
     .catch(error => console.log(error))
 }
 
-// module.exports = (req,res) => {
-
-//     const products = readJSON("products.json");
-
-//     const productsModify = products.filter((product) => {
-//       if (product.id === req.params.id) {
-//           existsSync(`./public/images/products/${product.MainImage}`) &&
-//           unlinkSync(`./public/images/products/${product.MainImage}`);
-//       }
-//       if (product.id === req.params.id) {
-//         product.images.forEach(image => {
-//           existsSync(`./public/images/products/${image}`) &&
-//           unlinkSync(`./public/images/products/${image}`);
-//         });
-//       }
-//       return product.id !== req.params.id;
-//     })
-
-//     writeJSON(productsModify, "products.json");
-
-//     return res.redirect("/users/admin");
-// }
 
 
