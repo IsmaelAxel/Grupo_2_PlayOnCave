4433f7f0c95b5c3b3b6a99728cc3dd9f153ad0d0
“Nahuel <nnahuell.gb@hotmail.com>
2023-11-04 19:37:38 -0300

    create and delete product

--- src/controllers/products/create.js
+++ src/controllers/products/create.js
@@ -1,10 +1,10 @@
-const {validationResult} = require('express-validator')
-const {existsSync, unlinkSync} = require('fs')
+const { validationResult } = require('express-validator')
+const { existsSync, unlinkSync } = require('fs')
 const db = require('../../database/models')
-module.exports = (req,res) => {
+module.exports = (req, res) => {
     const errors = validationResult(req)
-    if(errors.isEmpty()){
-        const {title, categoryId, price, discount, description, minOs, minProcessor, minMemory, minGraphicsCard, minDisk, recommendedOs, recommendedProcessor, recommendedMemory, recommendedGraphicsCard, recommendedDisk,sectionId} = req.body
+    if (errors.isEmpty()) {
+        const { title, categoryId, price, discount, description, minOs, minProcessor, minMemory, minGraphicsCard, minDisk, recommendedOs, recommendedProcessor, recommendedMemory, recommendedGraphicsCard, recommendedDisk, sectionId } = req.body
         db.Products.create({
             title: title.trim(),
             categoryId,
@@ -22,37 +22,49 @@ module.exports = (req,res) => {
             recommendedGraphicsCard: recommendedGraphicsCard.trim(),
             recommendedDisk: recommendedDisk.trim(),
             sectionId,
-            
-
         })
-        .then(product => {
-            if(req.files.mainImage){
-                db.Images.create({
-                    file :  req.files.mainImage[0].filename,
-                    main : true,
-                    productId : product.id
-                })
-                    .then( () => {
-                        if(req.files.images){
-                           const images = req.files.images.map(({filename}) => {
-                            return {
-                                file: filename,
-                                main : false,
-                                productId : product.id
-                            }
-                           })
+            .then(product => {
+                if (sectionId) {
+                    const sectionDB = sectionId.map(section => {
+                        return {
+                            productsId: product.id,
+                            sectionId: section
+                        }
+                    })
+                    db.Products_section.bulkCreate(sectionDB, {
+                        validate: true
+                    }).then(() => {
+                        return console.log('plataformas agregadas');
+                    })
 
-                           db.Images.bulkCreate(images,{
-                            validate : true
-                           }).then(result => console.log(result))
-                    }
-                })
-            }
-            return res.redirect('/users/admin')
-        }).catch(error => console.log(error))
-    }else{
-        (req.files.mainImage && existsSync(`./public/images/products/${req.files.mainImage[0].filename }`)) && unlinkSync(`./public/images/products/${req.files.mainImage[0].filename }`);
-        if(req.files.images) {
+                }
+                if (req.files.mainImage) {
+                    db.Images.create({
+                        file: req.files.mainImage[0].filename,
+                        main: true,
+                        productId: product.id
+                    })
+                        .then(() => {
+                            if (req.files.images) {
+                                const images = req.files.images.map(({ filename }) => {
+                                    return {
+                                        file: filename,
+                                        main: false,
+                                        productId: product.id
+                                    }
+                                })
+
+                                db.Images.bulkCreate(images, {
+                                    validate: true
+                                }).then(result => console.log(result))
+                            }
+                        })
+                }
+                return res.redirect('/users/admin')
+            }).catch(error => console.log(error))
+    } else {
+        (req.files.mainImage && existsSync(`./public/images/products/${req.files.mainImage[0].filename}`)) && unlinkSync(`./public/images/products/${req.files.mainImage[0].filename}`);
+        if (req.files.images) {
             req.files.images.forEach(file => {
                 existsSync(`./public/images/products/${file.filename}`) && unlinkSync(`./public/images/products/${file.filename}`)
             })
@@ -60,19 +72,20 @@ module.exports = (req,res) => {
         const category = db.Category.findAll({
             order: ['name']
         });
-    
+
         const sections = db.Section.findAll({
             order: ['name']
         });
-    
-        Promise.all([category,sections])
-        .then(([category,sections]) => {
-            return res.render('productAdd',{
-                category,
-                sections : sections,
-                errors : errors.mapped(),
-                old : req.body
-            }) 
-        }).catch(error => console.log(error))
+
+        Promise.all([category, sections])
+            .then(([category, sections]) => {
+                return res.render('productAdd', {
+                    category,
+                    sections: sections,
+                    errors: errors.mapped(),
+                    old: req.body
+                })
+            }).catch(error => console.log(error))
     }
+
 }
