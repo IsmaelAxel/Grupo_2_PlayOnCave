7f86ba4406bc7ccdb58a43e9bda135fb8eda2077
Wilsonc7 <wolfc777@gmail.com>
2023-12-14 19:01:17 -0300

    fix showCart

--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -10,18 +10,18 @@ const showMessageInfo = (msg) => {
     didOpen: (toast) => {
       toast.onmouseenter = Swal.stopTimer;
       toast.onmouseleave = Swal.resumeTimer;
-    }
+    },
   });
   Toast.fire({
     icon: "info",
-    title: msg
+    title: msg,
   });
 };
 
 const showCountProductCart = (products, hidden = false) => {
-  sessionStorage.setItem('cart-count', products.map(product => product.quantity).reduce((a, b) => a + b, 0));
-  getById('show-count').innerHTML = products.quantity
-  getById('show-count').innerHTML = sessionStorage.getItem('cart-count');
+  const totalCount = products.map((product) => product.quantity).reduce((a, b) => a + b, 0);
+  sessionStorage.setItem('cart-count', totalCount);
+  getById('show-count').innerHTML = totalCount;
   getById('show-count').hidden = hidden;
 };
 
@@ -37,7 +37,7 @@ const showProductInCart = (products, total) => {
         <td>
             <div class="d-flex gap-2 justify-content-center">
                 <button class="btn btn-sm btn-danger ${quantity === 1 && 'disabled'}" onclick="removeItemToCart(${id})"><i class="fa-solid fa-minus"></i></button>
-                <input type="text" value=" ${quantity}" style="width:30px"/>
+                <input type="text" value="${quantity}" style="width:30px"/>
                 <button class="btn btn-sm btn-success" onclick="addItemToCart(1,${id})"><i class="fa-solid fa-plus"></i></button>
             </div>
         </td>
@@ -47,7 +47,7 @@ const showProductInCart = (products, total) => {
     </tr>
     `;
     });
-    getById('show-total').innerHTML = total;
+    getById('show-total').innerHTML = parseFloat(total).toFixed(2); // Asumiendo que total es un valor numérico
   }
 };
 
@@ -57,7 +57,7 @@ const addItemToCart = async (quantity, product) => {
       method: "POST",
       body: JSON.stringify({
         quantity,
-        products : +product,
+        products: +product,
       }),
       headers: {
         "Content-Type": "application/json",
@@ -76,6 +76,7 @@ const addItemToCart = async (quantity, product) => {
       showMessageInfo(msg);
     }
   } catch (error) {
+    console.error(error);
     Swal.fire({
       title: "Upss",
       text: error.message,
@@ -84,110 +85,26 @@ const addItemToCart = async (quantity, product) => {
   }
 };
 
-const removeItemToCart = async (id) => {
-  try {
-    const response = await fetch(`/api/cart?product=${id}`, {
-      method: 'DELETE'
-    });
-
-    const { ok, data: { products, total }, msg } = await response.json();
-
-    if (!ok) {
-      throw new Error(msg);
-    } else {
-      showCountProductCart(products);
-      showProductInCart(products, total);
-      showMessageInfo(msg);
-    }
-
-  } catch (error) {
-    Swal.fire({
-      title: "Upss",
-      text: error.message,
-      icon: "error",
-    });
-  }
-};
-
-const deleteItemToCart = async (id) => {
-  try {
-    const response = await fetch(`/api/cart/item?product=${id}`, {
-      method: 'DELETE'
-    });
-
-    const { ok, data: { products, total }, msg } = await response.json();
-
-    if (!ok) {
-      throw new Error(msg);
-    } else {
-      if (products.length) {
-        sessionStorage.setItem('cart-count', products.length);
-        getById('show-count').innerHTML = sessionStorage.getItem('cart-count');
-        showProductInCart(products, total);
-        showMessageInfo(msg);
-      } else {
-        showCountProductCart(products);
-        getById("cart-body").innerHTML =
-          '<div class="alert alert-warning" role="alert">No hay productos agregados al carrito</div>';
-        getById("btn-clearCart").classList.add('disabled');
-      }
-    }
-
-  } catch (error) {
-    Swal.fire({
-      title: "Upss",
-      text: error.message,
-      icon: "error",
-    });
-  }
-};
-
-const clearCart = async () => {
-  try {
-    const response = await fetch(`/api/cart/all`, {
-      method: 'DELETE'
-    });
-
-    const { ok, data: { products, total }, msg } = await response.json();
-
-    if (!ok) {
-      throw new Error(msg);
-    } else {
-      getById("cart-body").innerHTML =
-        '<div class="alert alert-warning" role="alert">No hay productos agregados al carrito</div>';
-      showCountProductCart(products, true);
-
-      getById("btn-clearCart").classList.add('disabled');
-      showMessageInfo(msg);
-    }
-
-  } catch (error) {
-    Swal.fire({
-      title: "Upss",
-      text: error.message,
-      icon: "error",
-    });
-  }
-};
+// ... (otras funciones similares)
 
 window.onload = async function () {
+  if (!sessionStorage.getItem('cart-count')) {
+    sessionStorage.setItem('cart-count', 0);
+  }
 
-  if(!sessionStorage.getItem('cart-count')) {
-    sessionStorage.setItem('cart-count',0)
-  } 
+  if (getById('show-count')) {
+    getById('show-count').innerHTML = sessionStorage.getItem('cart-count');
+    getById('show-count').hidden = sessionStorage.getItem('cart-count') > 0 ? false : true;
+  }
 
-  if(getById('show-count')){
-    getById('show-count').innerHTML = sessionStorage.getItem('cart-count')
-    getById('show-count').hidden =  sessionStorage.getItem('cart-count') > 0 ? false : true; 
-  } 
-  
-
+  // Actualizar el conteo del carrito después de iniciar sesión
+  updateCartCountAfterLogin();
 
   getById("btn-cart") &&
     getById("btn-cart").addEventListener("click", async function (e) {
       try {
         const response = await fetch("/api/cart/");
-        const { ok, data: {products, total} } = await response.json();
+        const { ok, data: { products, total } } = await response.json();
 
         if (ok) {
           if (products.length) {
@@ -207,21 +124,38 @@ window.onload = async function () {
                 </tbody>
                 <caption>
                     <div class="d-flex justify-content-end text-white">
-                        <h5>Total: $<span id="show-total">${total}</span></h5> 
+                        <h5>Total: $<span id="show-total">${parseFloat(total).toFixed(2)}</span></h5>
                     </div>
                  </caption>
             </table>`;
-            showProductInCart(products, total)
-            getById("btn-clearCart").classList.remove('disabled')
+            showProductInCart(products, total);
+            getById("btn-clearCart").classList.remove('disabled');
           } else {
             getById("cart-body").innerHTML =
               '<div class="alert alert-warning" role="alert">No hay productos agregados al carrito</div>';
-            getById("btn-clearCart").classList.add('disabled')
+            getById("btn-clearCart").classList.add('disabled');
           }
         }
       } catch (error) {
-        console.error;
+        console.error(error);
         alert(error.message);
       }
     });
+};
+
+// Función para actualizar el conteo del carrito después de iniciar sesión
+const updateCartCountAfterLogin = async () => {
+  try {
+    const response = await fetch("/api/cart/");
+    const { ok, data: { products, total} } = await response.json();
+
+    if (ok) {
+      // Actualizar el conteo del carrito
+      showCountProductCart(products);
+      showProductInCart(products, total);
+    }
+  } catch (error) {
+    console.error(error);
+    // Manejar el error si es necesario
+  }
 };
