7b7ee3c8a31e963a38f54d0aa1ed132afca8c1f8
IsmaelAxel <ismael_callamullo@hotmail.com>
2023-12-19 19:39:48 -0300

    fix: carrousel y search bar

--- public/javascripts/validationsFrontEnd/searchValidator.js
+++ public/javascripts/validationsFrontEnd/searchValidator.js
@@ -1,9 +1,16 @@
-const form_search = document.getElementById('form_search')
-window.onload = function () {
-const search = document.getElementById('input_search')
-    form_search.addEventListener('submit', function(e){
-        if(search.value === ""){
-            e.preventDefault()
+document.addEventListener('DOMContentLoaded', function () {
+    const form_search = document.getElementById('form_search');
+    const search = document.getElementById('input_search');
+
+    function handleFormSubmit(e) {
+        if (search.value === "") {
+            e.preventDefault();
         }
-    })
-}
+    }
+
+    search.addEventListener('input', handleFormSubmit);
+
+    form_search.addEventListener('submit', function (e) {
+        handleFormSubmit(e);
+    });
+});
